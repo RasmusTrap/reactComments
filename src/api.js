59b13ff133d064d8facd7476ac57f680faca27cf
Rasmus Trap <rasm510f@stud.kea.dk>
2022-10-05 18:14:27 +0200

    first

---
+++ src/api.js
@@ -0,0 +1,55 @@
+export const getComments = async () => {
+  return [
+    {
+      id: "1",
+      body: "Det godt lavet",
+      username: "Janus K",
+      userId: "1",
+      parentId: null,
+      createdAt: "2021-08-16T23:00:33.010+02:00",
+    },
+    {
+      id: "2",
+      body: "Enig",
+      username: "Rasmus Trap",
+      userId: "2",
+      parentId: "1",
+      createdAt: "2021-08-16T23:00:33.010+02:00",
+    },
+    {
+      id: "3",
+      body: "Rigtig flot skrevent",
+      username: "Rasmus Møller",
+      userId: "2",
+      parentId: null,
+      createdAt: "2021-08-16T23:00:33.010+02:00",
+    },
+    {
+      id: "4",
+      body: "Undskyld for stavefejl",
+      username: "Rasmus Møller",
+      userId: "2",
+      parentId: "3",
+      createdAt: "2021-08-16T23:00:33.010+02:00",
+    },
+  ];
+};
+
+export const createComment = async (text, parentId = null) => {
+  return {
+    id: Math.random().toString(36).substr(2, 9),
+    body: text,
+    parentId,
+    userId: "1",
+    username: "Rasmus Trap",
+    createdAt: new Date().toISOString(),
+  };
+};
+
+export const updateComment = async (text) => {
+  return { text };
+};
+
+export const deleteComment = async () => {
+  return {};
+};
